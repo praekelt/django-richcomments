7d8334612a924a454785383d53cf87cebb2f16d1
shaunsephton <shaunsephton@gmail.com>
2010-06-14 19:40:11 +0200

    polish

--- setup.py
+++ setup.py
@@ -2,11 +2,22 @@ from setuptools import setup, find_packages
 
 setup(
     name='django-richcomments',
-    version='dev',
+    version='0.0.1',
     description='Django app extending the comments framework for RIA.',
-    author='Praekelt Consulting',
+    long_description = open('README.rst', 'r').read(),
+    author='Praekelt Foundation',
     author_email='dev@praekelt.com',
-    url='https://github.com/praekelt/django-richcomments',
+    license='BSD',
+    url='http://github.com/praekelt/django-richcomments',
     packages = find_packages(),
     include_package_data=True,
+    classifiers = [
+        "Programming Language :: Python",
+        "License :: OSI Approved :: BSD License",
+        "Development Status :: 4 - Beta",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+    ],
 )
