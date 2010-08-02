6b75cd442c8e48ac0f325c74fe464655e69ce9bb
shaunsephton <shaunsephton@gmail.com>
2010-08-02 08:37:02 +0200

    pass through complet context

--- richcomments/views.py
+++ richcomments/views.py
@@ -1,7 +1,7 @@
 from django import template
 from django.contrib.contenttypes.models import ContentType
 from django.http import HttpResponse
-from django.template import Template
+from django.template import Template, RequestContext
 
 def list(request, content_type, id):
     """
@@ -14,5 +14,7 @@ def list(request, content_type, id):
 
     # setup template and return result
     t = Template("{% load comments %}{% render_comment_list for object %}")
-    result = t.render(template.Context({'object': obj}))
+    context = RequestContext(request)
+    context.update({'object': obj})
+    result = t.render(context)
     return HttpResponse(result)
